ed9a1ceb9295d577cfbd06491080890f3ac3180b
ryota_13 <94586220+Asta-13@users.noreply.github.com>
2023-08-29 19:55:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 const kanjiList = [
-  { kanji: "一", meaning: "1" },
-  { kanji: "会", meaning: "会う、会社" },
-  { kanji: "日", meaning: "日本、今日" },
-  // 他の漢字も同様に追加
-  // ...
-  { kanji: "犬", meaning: "犬、ペット" }, // 例示のための一つ
-  // ...
-  { kanji: "食", meaning: "食べる、食事" }, // 例示のための一つ
-  // ...
-  // 111個の漢字を追加
+    // 漢字リストをここに追加
+    月
 ];
 
-const cardContainer = document.querySelector('.card-container');
+const kanjiContainer = document.getElementById('kanji-container');
+let currentKanjiIndex = 0;
 
-function createCard(kanji, meaning) {
-  const card = document.createElement('div');
-  card.classList.add('card');
-  card.innerHTML = `
-    <div class="front" onclick="flipCard(this)">${kanji}</div>
-    <div class="back" onclick="flipCard(this)">${meaning}</div>
-  `;
-  return card;
+function displayKanji(index) {
+    kanjiContainer.textContent = kanjiList[index];
 }
 
-function renderCards() {
-  kanjiList.forEach(item => {
-    const card = createCard(item.kanji, item.meaning);
-    cardContainer.appendChild(card);
-  });
-}
+displayKanji(currentKanjiIndex);
 
-renderCards();
+kanjiContainer.addEventListener('click', () => {
+    currentKanjiIndex = (currentKanjiIndex + 1) % kanjiList.length;
+    displayKanji(currentKanjiIndex);
+});
 
-function flipCard(card) {
-  card.classList.toggle('flipped');
-}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+
+
